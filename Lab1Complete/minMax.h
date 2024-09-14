40b8fd2877102aa0e31b393bea87d2af2c4be23c
ZipionLive <alexandre@zipion-live.be>
2024-09-14 18:47:48 +0200

    added project Lab1Complete

--- Lab1Complete/minMax.h
+++ Lab1Complete/minMax.h
@@ -5,7 +5,7 @@
 #ifndef MINMAX_H
 #define MINMAX_H
 
-void showMin(int *tab, int size);
-void showMax(int *tab, int size);
+int getMin(int *tab, int size);
+int getMax(int *tab, int size);
 
 #endif //MINMAX_H
